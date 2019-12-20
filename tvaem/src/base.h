d2ec139de843903245572c8d75dfd41316676f73
Vadim Petrov <vadim.vooku.petrov@gmail.com>
2019-12-20 13:40:32 -0500

    Bump version to 0.6.2

--- tvaem/src/base.h
+++ tvaem/src/base.h
@@ -6,5 +6,5 @@ typedef uint8_t midiNote;
 static const int MAX_LAYERS = 8;
 static const int MAX_EFFECTS = 5;
 static const float MAX_7BITF = 127.0f;
-static constexpr const char * VERSION = "0.6.0-alpha";
+static constexpr const char * VERSION = "0.6.2-alpha";
 static const midiNote DEFAULT_MASTER_ALPHA_CONTROL = 16;
