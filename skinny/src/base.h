f2d1fffa562f1e196da40a092432a0fcf25a86c0
vooku <vadim.vooku.petrov@gmail.com>
2020-03-29 20:53:45 +0200

    Bump version to 0.7.3

--- skinny/src/base.h
+++ skinny/src/base.h
@@ -8,7 +8,7 @@ typedef uint8_t midiNote;
 static const int MAX_LAYERS = 8;
 static const int MAX_EFFECTS = 5;
 static const float MAX_7BITF = 127.0f;
-static constexpr const char* VERSION = "0.7.2-alpha";
+static constexpr const char* VERSION = "0.7.3-alpha";
 static constexpr const char* NAME = "Skinny Mixer";
 static constexpr const char* AUTHOR = "Vadim Vooku Petrov";
 static const midiNote DEFAULT_MASTER_ALPHA_CONTROL = 16;
